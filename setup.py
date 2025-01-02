3accf1dd12757837d0287968fafcfef1a0d8d54e
maxtico <max.tico@alumni.esci.upf.edu>
2025-01-02 15:26:03 +0100

    action workflow to ubuntu:24.04

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="pyranges_plot",
-    version="0.1.0",
+    version="0.1.2",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,  # Ensure package data is included
